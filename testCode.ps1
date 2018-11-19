d873943465e978f1cd2e2c87d92fe9befb9d3df4
MattBoren <matt@cool.com>
2018-11-19 16:05:26 -0500

    add some test code, update readme

---
+++ testCode.ps1
@@ -0,0 +1,10 @@
+<#	.Description
+	Some code to test version control actions
+
+	.Example
+	testCode -Verbose
+	Get some verbose message
+#>
+[CmdletBinding()]
+param()
+Write-Verbose "Cool message, man -- you have the version control, now?"
